e85c4903e5218a250ea5936e0290587041ced23e
alfred-egbon <egbon.o.a@gmail.com>
2022-09-08 08:25:59 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
-
 /**
-* main - print the string in the put function
-*
-*Description: using the mian function
-*this program prints "Programming is like building a multilingual puzzle
-*return:0
-*/
+ * main - entry point
+ *
+ *Description: using the mian function
+ *this program prints "Programming is like building a multilingual puzzle
+ *return: 0 after printing the function 
+ */
 int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
+{puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
