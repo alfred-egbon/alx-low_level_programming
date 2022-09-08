001cf7a256fca770129e1ab7799d0072c5a373c3
alfred-egbon <egbon.o.a@gmail.com>
2022-09-08 08:29:53 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - entry point
  *
- *Description: using the mian function
- *this program prints "Programming is like building a multilingual puzzle
+ *Description: using the main function
+ *this program prints "Programming is like building a multilingual puzzle"
  *return: 0 after printing the function 
- */
+*/
 int main(void)
 {puts("\"Programming is like building a multilingual puzzle");
 	return (0);
