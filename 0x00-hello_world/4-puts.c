e5f1021380cedbb9efa923a846c7dd8d03948bf4
alfred-egbon <egbon.o.a@gmail.com>
2022-09-09 04:30:09 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,11 +5,11 @@
  *
  * Description: 'Programming is like building a multilingual puzzle'
  *
- * Return: Always 0 (Success)
+ * Return: iAlways 0 (Success)
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
