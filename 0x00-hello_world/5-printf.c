7716bfb03986550196087760fc60b77fac3771e5
alfred-egbon <egbon.o.a@gmail.com>
2022-09-09 04:54:19 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - print entry
  *
  * Description: 'Programming is like building a multilingual puzzle'
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (success)
  */
 
 int main(void)
 {
-       	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
