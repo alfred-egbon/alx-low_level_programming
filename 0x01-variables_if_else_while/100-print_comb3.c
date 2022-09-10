6afb45fb274eb140ab9c811197b68db4a78b1a11
alfred-egbon <egbon.o.a@gmail.com>
2022-09-09 19:00:18 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,22 +7,25 @@
  */
 int main(void)
 {
-        int d;
+       int i, j;
+       for (i = 0; i <= 9; i++)
+       {
+	       for (j = 1; j <= 9; j++)
+	       {
+		       if (j > i)
+		       {
 
-        for (d = 0; d < 100; d++)
-        {
-                putchar((d / 10) + '0');
+			       putchar(i + '0');
+			       putchar(j + '0');
+			       if (i != 8)
 
-                                putchar((d % 10) + '0');
-
-                                if (d != 89)
-                                {
-                                        putchar(',');
-                                        putchar(' ');
-                                }
-        }
-        putchar('\n');
-
-        return (0);
+			       {
+				       putchar(',');
+				       putchar(' ');
+			       }
+		       }
+	       }
+       }
+       putchar('\n');
+       return (0);
 }
-
