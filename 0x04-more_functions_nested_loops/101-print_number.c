77fd2453d83b8c257c75fd152409530508cdf86b
alfred-egbon <egbon.o.a@gmail.com>
2022-09-15 13:04:20 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -18,8 +18,8 @@ void print_number(int n)
 	if (n == 0)
 		putchar('0');
 
-	if (n/10)
-		print_number(n/10);
+	if (n / 10)
+		print_number(n / 10);
 
-	putchar(n%10 + '0');
+	putchar(n % 10 + '0');
 }
